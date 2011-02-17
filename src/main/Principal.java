f78e8307f1d483d38951644a3ed0c8f87a100025
Leo <leokury@gmail.com>
2011-02-17 11:24:04 -0300

    teste

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,24 +1,12 @@
 package main;
 
-import java.util.Random;
-
-import nmf.NMF;
-
-import org.ejml.data.SimpleMatrix;
-
 public class Principal {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		SimpleMatrix sm = SimpleMatrix.random(5, 5, 0, 1, new Random());
-		sm.set(3, 3, 0);
-		sm.print();
-		
-		System.out.println("--------Resultado-----------");
-		NMF.nmf(sm, 3).produto().print();
+		System.out.println("teste");
 	}
 
 }
